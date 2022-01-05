a1e44ab7a08c7404ba93e86535a33d59fddcd142
Adeline <adeline.mdg@gmail.com>
2022-01-05 15:05:55 +0100

    up, ajout du nbr d'inscriptions

---
+++ inscriptionsmultiples.py
@@ -0,0 +1,31 @@
+from mesfonctions import *
+
+inscrits = int(input("indiquez le nombre de personnes à inscrire\n"))
+
+leslistes = {"poussins":[],"cadets":[],"junior":[],"semipro": [],"pro":[]}
+
+
+for i in range (inscrits):
+    inscription = {}
+    inscription["nom"] = input("Ecrivez le nom\n")
+    inscription["prenom"] = input("Ecrivez le prénom\n")
+    inscription["year"] = int(input("Ecrivez année de naissance\n"))
+
+    email = Mail(inscription["nom"][0], inscription["prenom"])
+    inscription["email"] = email
+
+    if 6 <= age(inscription["year"]) < 12:
+        leslistes["poussins"].append(inscription)
+    elif 12 <= age(inscription["year"]) < 18:
+        leslistes["cadets"].append(inscription)
+    elif 18 <= age(inscription["year"]) < 24:
+        leslistes["junior"].append(inscription)
+    elif 24 <= age(inscription["year"]) < 30:
+        leslistes["semipro"].append(inscription)
+    elif 30 <= age(inscription["year"]) <= 40:
+        leslistes["pro"].append(inscription)
+    else:
+        print("NON ADMIS")
+
+
+print(leslistes)
