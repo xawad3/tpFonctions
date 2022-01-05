21a149526af96bdbf1b0daa636678ef13f07be21
Adeline <adeline.mdg@gmail.com>
2022-01-05 11:27:23 +0100

    update fonctionalité 1

--- inscription.py
+++ inscription.py
@@ -15,8 +15,19 @@ def age(annee):
     return 2022-annee
 
 
-if age(inscription["year"]) < 18:
+if 6 <= age(inscription["year"]) < 12:
     poussin.append(inscription)
+elif 12 <= age(inscription["year"]) < 18:
+    cadet.append(inscription)
+elif 18 <= age(inscription["year"]) < 24:
+    junior.append(inscription)
+elif 24 <= age(inscription["year"]) < 30:
+    semipro.append(inscription)
+elif 30 <= age(inscription["year"]) <= 40:
+    pro.append(inscription)
+else:
+    print("NON ADMIS")
+
 
 print(poussin)
 print(age(inscription["year"]))
